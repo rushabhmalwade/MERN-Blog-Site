63314a01258f17f3b91e59837ea60cdc3deaf99b
RishabhMalwade <rushabhmalwade@gmail.com>
2024-07-17 10:20:17 +0530

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import { Header } from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="projects" element={<Projects />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,14 @@
+import flowbite from "flowbite-react/tailwind"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_module/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [flowbite.plugin()],
+};
